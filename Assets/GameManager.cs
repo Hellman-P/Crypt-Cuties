6308fad1622a31eb195dfef2acd3c47873864a91
Hellman-P <hellmanp98@gmail.com>
2023-05-12 05:55:42 +0200

    pleswork

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public Vector3[] spawners;
     public GameObject enemy;
+    private float spawnRate = 0.8f;
+
+    public TextMeshProUGUI scoreText;
+    private int score;
+
+    public TextMeshProUGUI levelText;
+    private int level;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < spawners.Length; i++)
-        {
-            Instantiate(enemy, spawners[i], enemy.transform.rotation);
-        }
+        score = 0;
+        scoreText.text = "KILLS: " + score;
+
+        level = 0;
+        levelText.text = "LEVEL: " + level;
+
+        StartCoroutine(SpawnEnemies());
     }
 
     // Update is called once per frame
@@ -22,8 +33,15 @@ public class GameManager : MonoBehaviour
         
     }
 
-    public void spawnEnemies()
+    IEnumerator SpawnEnemies()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            for (int i = 0; i < spawners.Length; i++)
+            {
+                Instantiate(enemy, spawners[i], enemy.transform.rotation);
+            }
+        }
     }
 }
